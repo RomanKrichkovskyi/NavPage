c658ae4f436c64bf899b0c10da942a0abaffadb5
DESKTOP-KB22PM6\roman <rkrichkovsky@gmail.com>
2018-03-10 13:56:58 +0200

    Navigation title
    Creating new project in Visual Studio 2017. Show nevigation title.

---
+++ NavPage/NavPage/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace NavPage
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+            var mp = new NavigationPage(new NavPage.MainPage { Title = "MAinPage" });
+            mp.Navigation.PushAsync(new MainPage{ Title = "SoftBide" });
+            MainPage = mp;
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
